78e2a4739fe80767af5d541243c79bb4eb8a5ef3
Joe Cowton <joecowton@gmail.com>
2018-02-22 22:34:09 +0000

    every pallindrome soloution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+	return str.split('').every((char, i) => {
+		return char === str[str.length - i - 1];
+	});
+}
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+// 	let reversed = '';
+//
+// 	for (let characters of str) {
+// 		reversed = characters + reversed;
+// 	}
+//
+// 	return reversed === str;
+// }
